2c5f36bc5a6f4deea09871e0a5c8399ab606de63
Alexey Petin <alex_beholder@mail.ru>
2023-01-30 22:51:00 +0300

    Append solution

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.io.*;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String path = "D:/Games/savegames/saves.zip";
+        String pathToUnpack = "D:/Games/savegames";
+        openZip(path, pathToUnpack);
+        System.out.println(openProgress(pathToUnpack).toString());
+    }
+
+    public static void openZip(String path, String pathToUnpack) {
+        try (ZipInputStream zis = new ZipInputStream(new FileInputStream(path))) {
+            ZipEntry entry;
+            while((entry = zis.getNextEntry()) != null){
+                String name = new File(entry.getName()).getName();
+                FileOutputStream fos = new FileOutputStream(pathToUnpack.concat("/" + name));
+                int c;
+                while ((c = zis.read()) != -1){
+                    fos.write(c);
+                }
+                fos.flush();
+                zis.closeEntry();
+                fos.close();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static GameProgress openProgress(String path){
+        GameProgress gp = null;
+        try(FileInputStream fis = new FileInputStream(path.concat("/save1.dat"));
+            ObjectInputStream ois = new ObjectInputStream(fis)){
+            gp = (GameProgress) ois.readObject();
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return gp;
     }
 }
